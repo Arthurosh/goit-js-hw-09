3d770e5ca58e72a6bd4712cff0db70698e5b86b7
ArthurS <arthurosh@gmail.com>
2022-09-29 09:35:34 +0300

    adds the executions of task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const bgColor = function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+let timerId = null;
+
+refs.btnStart.addEventListener('click', onStart);
+refs.btnStop.addEventListener('click', onStop);
+
+function onStart() {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = bgColor();
+  }, 1000);
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+}
+function onStop() {
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+  clearInterval(timerId);
+}
